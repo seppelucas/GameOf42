21709e09a290e973186ff66f489e9e3cc2284b33
seppe lucas <seppelucas@hotmail.com>
2020-10-08 12:01:29 +0200

    v1.1

--- game.c
+++ game.c
@@ -36,9 +36,66 @@ void scaninput(){
 
 }
 
+int options2(int a, int b, int i2){
+
+	//for(;i2<4;i2++){
+		if(i2 ==0){
+			return a+b;
+		}
+		if(i2 ==1){
+			return a-b;
+		}
+		if(i2 ==2){
+			return a/b;
+		}
+		if(i2 ==3){
+			return a*b;
+		}
+	//}
+}
+
+void solvegame(int array[4]){
+
+	int total =0;
+	int k=0,l=0,o=0, scanning =1;
+
+	while(scanning){
+		if(24 == options2(options2(array[0],array[1],l),options2(array[2],array[3],o),k) ){
+			printf("yes\n");
+			scanning =0;
+		}
+		else{
+			k++;
+			if(k == 4){
+				l++;
+				k=0;
+				if(l ==4 ){
+					o++;
+					l=0;
+					if(o ==4){
+						printf("no solution for now");
+						scanning =0;
+					}
+				}
+			}
+		}
+
+		printf("k=%d l=%d o=%d\n",k,l,o);
+			
+	}
+	}
+
+
+
+
+
+
 int main(int argc, char *argv[]){ 
 
 	scaninput();
+
+	// four operators + - * / 
+	solvegame(array);
 
 
 }
