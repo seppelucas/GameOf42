158fe73aca2fca64012aa24ab7fe9e095bf95b35
seppe lucas <seppelucas@hotmail.com>
2020-10-08 10:41:39 +0200

    added input

--- game.c
+++ game.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-int i;
+
 
 int array[4];
 
@@ -9,11 +9,29 @@ int array[4];
 
 void scaninput(){
 	printf("This is a game of 24 solver\nPlease input the 4 numbers you found in the deck\n");
-	for(i=0;i<4;i++){
+	for(int i=0;i<4;i++){
 		scanf("%d",&array[i]);
 	}
-	printf("your four numbers are %d %d %d %d\n",array[0], array[1], array[2], array[3]);
+	printf("your four numbers are: %d %d %d %d\nIs this correct? (y/n)",array[0], array[1], array[2], array[3]);
+
 	// add yes/no option with second input possibility
+	char ch;
+	scanf("%c\n",&ch);
+	scanf("%c",&ch);
+
+
+
+	if(ch == 'n'){
+
+		printf("\nPlease try again\n");
+		scaninput();
+	}
+
+
+
+
+
+
 	// add check that all 4 numbers are in a deck.
 
 
